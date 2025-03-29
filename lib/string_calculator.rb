8441cb6c313a97fea225b50c509731fddb776a9f
prashu <prashu@foradian.com>
2025-03-29 14:08:36 +0530

    Feat: Handle single number input

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,6 @@
 class StringCalculator
     def add(numbers)
-      return 0 if numbers.empty?
-    end
+        return 0 if numbers.empty?
+        return numbers.to_i if numbers.match(/^\d+$/)
+      end
   end
