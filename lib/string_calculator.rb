ffd8917788c6d40c4b92fd55dc0cd668461789de
prashu <prashu@foradian.com>
2025-03-29 16:17:33 +0530

    Feat: Handle custom delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,9 +1,12 @@
 class StringCalculator
     
 	def add(numbers)
-		return 0 if numbers.empty?
-		return numbers.to_i if numbers.match(/^\d+$/)
-		numbers.split(/[,]/).map(&:to_i).sum
-	end
-
+    return 0 if numbers.empty?
+    return numbers.to_i if numbers.match(/^\d+$/)
+    delimiter = numbers.start_with?("//") ? numbers[2] : /[,]/
+    numbers = numbers.split("\n").last if numbers.start_with?("//")
+    numbers.split(delimiter).map(&:to_i).sum
   end
+
+end
+	
