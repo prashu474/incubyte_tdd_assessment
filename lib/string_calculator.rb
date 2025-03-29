1419672c70ec7626ab1aa1f7bf014c62d34abffd
prashu <prashu@foradian.com>
2025-03-29 16:07:13 +0530

    Feat: Handle sum of multiple numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,9 @@
 class StringCalculator
-    def add(numbers)
-      return 0 if numbers.empty?
-      return numbers.to_i if numbers.match(/^\d+$/)
-      numbers.split(',').map(&:to_i).sum
-    end
+    
+	def add(numbers)
+		return 0 if numbers.empty?
+		return numbers.to_i if numbers.match(/^\d+$/)
+		numbers.split(/[,]/).map(&:to_i).sum
+	end
+
   end
