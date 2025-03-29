4a68dc5eaf1ab8ff2a3705dd813faa0babaec3f2
prashu <prashu@foradian.com>
2025-03-29 16:30:08 +0530

    Feat: Raise error for negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,11 +1,15 @@
 class StringCalculator
     
-	def add(numbers)
+  def add(numbers)
     return 0 if numbers.empty?
     return numbers.to_i if numbers.match(/^\d+$/)
     delimiter = numbers.start_with?("//") ? numbers[2] : /[,]/
     numbers = numbers.split("\n").last if numbers.start_with?("//")
-    numbers.split(delimiter).map(&:to_i).sum
+    num_array = numbers.split(delimiter).map(&:to_i)
+    negatives = num_array.select { |num| num < 0 }
+    raise "negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
+	num_array.sum
+     end
   end
 
 end
