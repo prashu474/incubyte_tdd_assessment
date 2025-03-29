d8a321385aa73980ba50a1d2e0674898cd9488fe
prashu <prashu@foradian.com>
2025-03-29 16:28:09 +0530

    Refactor: Fix indentation for spec file

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,23 +13,23 @@ describe StringCalculator do
       expect(calculator.add("1")).to eq(1)
     end
 
-		it "returns sum of two numbers" do
-      calculator = StringCalculator.new
+      it "returns sum of two numbers" do
+        calculator = StringCalculator.new
       expect(calculator.add("1,2")).to eq(3)
     end
 
-		it "returns sum of multiple numbers" do
-      calculator = StringCalculator.new
+      it "returns sum of multiple numbers" do
+        calculator = StringCalculator.new
       expect(calculator.add("1,2,3,4")).to eq(10)
     end
 
-		it "supports different delimiters" do
-      calculator = StringCalculator.new
+      it "supports different delimiters" do
+        calculator = StringCalculator.new
       expect(calculator.add("//;\n1;2")).to eq(3)
     end
 
-		it "raises error for negative numbers" do
-      calculator = StringCalculator.new
+      it "raises error for negative numbers" do
+       calculator = StringCalculator.new
       expect { calculator.add("1,-2,3") }.to raise_error("negative numbers not allowed: -2")
     end
 
